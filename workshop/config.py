71862ff73e9f35d1604e477ab0af0c750c2abdc2
alexmillerdb <alexmillerdb>
2024-11-04 19:22:17 +0000

    update config

--- workshop/config.py
+++ workshop/config.py
@@ -13,3 +13,15 @@ config['volumes_table_path'] = f"{catalog}.{schema}.{volume}"
 config['output_dir_train'] = f"{config['volumes_path']}/mds_train"
 config['output_dir_validation'] = f"{config['volumes_path']}/mds_validation"
 config['output_dir_test'] = f"{config['volumes_path']}/mds_test"
+
+# COMMAND ----------
+
+print(f"Catalog: {catalog}")
+print(f"Schema: {schema}")
+print(f"Volumes path: {config['volumes_path']}")
+
+# COMMAND ----------
+
+spark.sql(f"CREATE CATALOG IF NOT EXISTS {catalog}")
+spark.sql(f"CREATE SCHEMA IF NOT EXISTS {catalog}.{schema}")
+spark.sql(f"CREATE VOLUME IF NOT EXISTS {catalog}.{schema}.{volume}")
